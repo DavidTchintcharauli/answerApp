3a0f73bf3f4dbe6979b7e7b2d2d13112d8e8dd9d
David Tchintcharauli <92579774+DavidTchntcharauli@users.noreply.github.com>
2023-05-26 01:05:32 +0400

    and one

---
+++ src/components/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+import { Operator } from "./operator"
+import { Game } from "./game"
+
+export type TyOperators = '+' | '-' | 'X' | '/'
+
+export const App = () => {
+
+  const [operatorList, setOperatorList] = useState<TyOperators[]>(["+"])
+  const [startGame, setStartGame] = useState(false)
+
+  if(!startGame) {
+    return <Operator
+      operatorList={operatorList} setOperatorList={setOperatorList}
+      setStartGame={setStartGame}
+    />
+  }
+
+  return <Game operatorList={operatorList} setStartGame={setStartGame} />
+}
+
+export default App;
